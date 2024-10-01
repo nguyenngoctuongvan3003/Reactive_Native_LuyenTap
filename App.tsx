8c88c75a7ad49c325c2b8db2515ddb27c4fc03b2
TuongVan <nguyenngoctuongvan3003@gmail.com>
2024-10-01 21:07:51 +0700

    todoapp +icon +flex

--- App.tsx
+++ App.tsx
@@ -13,8 +13,8 @@ import {
   TouchableWithoutFeedback,
   View,
 } from "react-native";
-import flexBox from "./components/flexBox";
 import FlexBox from "./components/flexBox";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
 interface ITodo {
   id: number;
   name: string;
@@ -43,60 +43,73 @@ export default function App() {
     setToDo("");
   };
   const deleteToDo = (id: number) => {
-    const newToDos = listToDo.filter((item) => item.id !== id);
-    setListToDo(newToDos);
+      const newToDos = listToDo.filter((item) => item.id !== id);
+      setListToDo(newToDos);
   };
 
   return (
-    <FlexBox/>
-    // <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
-    //   <View style={styles.container}>
-    //     {/* header */}
-    //     <Text style={styles.header}>Hehehe</Text>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        {/* header */}
+        <Text style={styles.header}>Hehehe</Text>
 
-    //     {/* form  */}
-    //     <View style={styles.body}>
-    //       <TextInput
-    //         value={toDo}
-    //         style={styles.toDoInput}
-    //         onChangeText={(value) => setToDo(value)}
-    //       />
-    //       <Button title="Add to do" onPress={handleAddTodo} />
-    //     </View>
+        {/* form  */}
+        <View style={styles.form}>
+          <TextInput
+            value={toDo}
+            style={styles.toDoInput}
+            onChangeText={(value) => setToDo(value)}
+          />
+          <Button title="Add to do" onPress={handleAddTodo} />
+        </View>
 
-    //     {/* list to do  */}
-    //     <View style={styles.body}>
-    //       <FlatList
-    //         data={listToDo}
-    //         keyExtractor={(item) => item.id + ""}
-    //         renderItem={({ item }) => {
-    //           return (
-    //             <Pressable
-    //               style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
-    //               onPress={() => deleteToDo(item.id)}
-    //             >
-    //               <Text style={styles.toDoItem}>{item.name}</Text>
-    //             </Pressable>
-    //           );
-    //         }}
-    //       />
-    //     </View>
-    //   </View>
-    // </TouchableWithoutFeedback>\
+        {/* list to do  */}
+        <View style={styles.toDo}>
+          <FlatList
+            data={listToDo}
+            keyExtractor={(item) => item.id + ""}
+            renderItem={({ item }) => {
+              return (
+                <Pressable
+                  style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
+                  
+                >
+                  <View style={styles.groupToDo}>
+                    <Text style={styles.toDoItem}>{item.name}</Text>
+                    <EvilIcons name="close" size={24} color="black" onPress={() => deleteToDo(item.id)} />
+                  </View>
+                </Pressable>
+              );
+            }}
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 // dùng vòng map để tạo ra 1 array mới mà không làm thay đổi giá trị của biến ban đầu
 const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: "red",
+    paddingTop: 50,
+    flex: 1,
+    backgroundColor: "#fff",
+  },
   header: {
     paddingHorizontal: 20,
     textAlign: "center",
     backgroundColor: "orange",
     fontSize: 40,
+    // flex:1
   },
-  container: {
-    paddingTop: 50,
+  form: {
+    // flex:2
+    marginBottom: 30,
+  },
+  toDo: {
     flex: 1,
-    backgroundColor: "#fff",
+   
   },
   list: {
     padding: 30,
@@ -115,8 +128,16 @@ const styles = StyleSheet.create({
   },
   toDoItem: {
     fontSize: 30,
+    
+  },
+  groupToDo: {
+    flexDirection: "row",
+    alignItems:"center" ,
     borderWidth: 1,
     borderStyle: "dashed",
-    marginBottom: 20,
-  },
+    marginBottom:15,
+    justifyContent:"space-between",
+    padding:15,
+    marginHorizontal:10
+  }
 });
